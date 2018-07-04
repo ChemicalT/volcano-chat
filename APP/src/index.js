458be9f899cdb4a2f150c0a219084239c308a831
Terence Blue <tblue@bu.edu>
2018-07-04 17:37:06 -0400

    Loader

--- APP/src/index.js
+++ APP/src/index.js
@@ -199,7 +199,7 @@ class View extends React.Component {
     'Notification' in window && Notification.requestPermission()
     existingUser
       ? ChatManager(this, JSON.parse(existingUser))
-      : fetch('https://volcano-chat.herokuapp.com/auth', {
+      : fetch('https://chatkit-demo-server.herokuapp.com/auth', {
           method: 'POST',
           body: JSON.stringify({ code: authCode }),
         })
@@ -285,7 +285,7 @@ const existingUser = window.localStorage.getItem('chatkit-user')
 const githubAuthRedirect = () => {
   const client = '20cdd317000f92af12fe'
   const url = 'https://github.com/login/oauth/authorize'
-  const server = 'https://chatkit-demo-server.herokuapp.com'
+  const server = 'https://volcano-chat.herokuapp.com'
   const redirect = `${server}/success?url=${window.location.href.split('?')[0]}`
   window.location = `${url}?scope=user:email&client_id=${client}&redirect_uri=${redirect}`
 }
